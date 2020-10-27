9756da6dcebea1569a2b4971041519256c589696
Chris <cec213@scarletmail.rutgers.edu>
2020-10-27 18:58:15 -0400

    mongodb atlas deploying

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
-  useNewUrlParser: true,
-  useFindAndModify: false
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
 
 });
 
